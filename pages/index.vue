002086f90368d1e7b9d21e59084a0d346961d037
Richard Mulero <mulerolp@gmail.com>
2022-02-05 02:05:49 -0300

    feat(login): iniciando login

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,46 @@
 <template>
-  <h1>Login</h1>
+  <el-row>
+    <el-col :span="8" :offset="8">
+      <el-card shadow="never" class="card-center" :body-style="{ padding: '3.75rem' }">
+        <el-row>
+          <img src="~/assets/icons/devchallenges.svg" />
+        </el-row>
+
+        <el-row>
+          <h3>Login</h3>
+        </el-row>
+
+        <el-row>
+          <el-input placeholder="Email" prefix-icon="el-icon-search" v-model="user.email"> </el-input>
+        </el-row>
+
+        <el-row>
+          <el-input placeholder="Password" prefix-icon="el-icon-search" v-model="user.password"> </el-input>
+        </el-row>
+
+        <el-row>
+          <el-button type="primary">Login</el-button>
+        </el-row>
+      </el-card>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
 export default {
-  name: "LoginPage"
+  name: "LoginPage",
+
+  data() {
+    return {
+      user: { email: "", password: "" }
+    };
+  }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card-center {
+  border-color: #bdbdbd;
+  border-radius: 24px;
+}
+</style>
